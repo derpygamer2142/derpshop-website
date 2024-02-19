db7b6165f9fb79de33029b4fbfb63932094b5158
derpygamer2142 <derpygamer2142@gmail.com>
2024-02-18 18:52:28 -0800

    minor coolness changes

--- shop/offer/script.js
+++ shop/offer/script.js
@@ -4,32 +4,46 @@ const form = document.getElementById("inputStuff")
 async function buttonStuff() {
     let username = document.getElementById("usernameInput").value
     let offer = document.getElementById("offerInput").value
-    const urlParams = new URLSearchParams(window.location.search);
-    const myParam = urlParams.get('item');
-    let item = decodeURIComponent(myParam)
-    console.log("button clicked")
-    const response = await fetch("https://discord.com/api/webhooks/1208875958157705237/vih84fRSS-lhSjwDlAsgKmGpIlS4W3uYy_2Un2eZkaBVLWZ_IbtvMOmxsprDiMKY2Wr4",{
-        "method": "POST",
-        "mode": "cors",
-        "cache": "no-cache",
-        "credentials": "same-origin",
-        "headers": {
-            "Content-Type": "application/json",
-          },
-        "body": JSON.stringify({
-            "content": `<@816743268707860511> \"${username}\" is offering the following for **\"${item}\"**:\n${offer}`,
-            "username": "Offer bot",
-            "allowed_mentions": {
-                "parse": ["everyone"],
-                "users": ["816743268707860511"]
-              }
+    console.log(offer)
+    if ((offer != "") && (username != "")) {
+        document.getElementById("usernameInput").value = ""
+        document.getElementById("offerInput").value = ""
+        const urlParams = new URLSearchParams(window.location.search);
+        const myParam = urlParams.get('item');
+        let item = decodeURIComponent(myParam)
+        console.log("button clicked")
+        const response = await fetch("https://discord.com/api/webhooks/1208875958157705237/vih84fRSS-lhSjwDlAsgKmGpIlS4W3uYy_2Un2eZkaBVLWZ_IbtvMOmxsprDiMKY2Wr4",{
+            "method": "POST",
+            "mode": "cors",
+            "cache": "no-cache",
+            "credentials": "same-origin",
+            "headers": {
+                "Content-Type": "application/json",
+                },
+            "body": JSON.stringify({
+                "content": `<@816743268707860511> \"${username}\" is offering the following for **\"${item}\"**:\n${offer}`,
+                "username": "Offer bot",
+                "allowed_mentions": {
+                    "parse": ["everyone"],
+                    "users": ["816743268707860511"]
+                    }
+            })
+
         })
+        let data = await response.text();
+        console.log(`Request responded with \"${data}\"`)
+        console.log("click")
 
-    })
-    let data = await response.text();
-    console.log(`Request responded with \"${data}\"`)
-    console.log("click")
+        let heldElement = document.createElement("h1")
+        heldElement.textContent = "Sent! You should recieve a response soon, otherwise send a followup."
+        let body = document.getElementById("main")
+        body.appendChild(heldElement)
+        alert("Sent! You should recieve a response soon, otherwise send a followup.")
+        
+        window.location.href = "/"
+    }
 
+    
 }
 
 const button = document.getElementById("submitButton")
